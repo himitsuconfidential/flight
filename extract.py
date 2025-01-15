b30e1ef889ac2b6c7809b5d1d718bda4700dd517
himitsuconfidential <aperson328@gmail.com>
2025-01-15 15:34:11 +0800

    fix bug for checkbox click

--- extract.py
+++ extract.py
@@ -21,10 +21,10 @@ def run(page, querystring, first_run=False):
     page.goto(f'https://hk.trip.com/flights/showfarefirst?dcity={dcity}&acity={acity}&ddate={ddate}&triptype=ow&class=y&lowpricesource=searchform&quantity=1&searchboxarg=t&nonstoponly=off&locale=zh-TW&curr=HKD', timeout=60000)
     # Wait for the page to load completely with a timeout of 60 seconds
     page.wait_for_load_state('networkidle', timeout=60000)  # 60 seconds in milliseconds
-    txt = "document.querySelectorAll('[data-code=\"DIRECT\"]')[0].querySelector('span').click()"
+    txt = "if (document.querySelectorAll('[data-code=\"DIRECT\"]')[0].querySelector('span.fui-checked') === null) document.querySelectorAll('[data-code=\"DIRECT\"]')[0].querySelector('span').click()"
     if (first_run):
         page.wait_for_timeout(9000)
-        page.evaluate(txt)
+    page.evaluate(txt)
     page.wait_for_timeout(1000)
     scroll_down(page)
     # Get the HTML content of the fully rendered page
