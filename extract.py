ef95a2226c4211ee3aea5503516df3be5a001bc5
himitsuconfidential <80973412+himitsuconfidential@users.noreply.github.com>
2025-07-05 15:08:44 +0800

    temp debug

--- extract.py
+++ extract.py
@@ -20,6 +20,9 @@ def run(page, querystring, first_run=False):
     #e.g. https://hk.trip.com/flights/showfarefirst?dcity=hkg&acity=tyo&ddate=2024-09-01&triptype=ow&class=y&lowpricesource=searchform&quantity=1&searchboxarg=t&nonstoponly=off&locale=zh-TW&curr=HKD
     page.goto(f'https://hk.trip.com/flights/showfarefirst?dcity={dcity}&acity={acity}&ddate={ddate}&triptype=ow&class=y&lowpricesource=searchform&quantity=1&searchboxarg=t&nonstoponly=off&locale=zh-TW&curr=HKD', timeout=60000)
     # Wait for the page to load completely with a timeout of 60 seconds
+    with open('extractsnapshot.html', 'w') as file: #for debug
+        print(page.content())
+        file.write(page.content())
     page.wait_for_load_state('networkidle', timeout=60000)  # 60 seconds in milliseconds
     #txt = "if (document.querySelectorAll('[data-code=\"DIRECT\"]')[0].querySelector('span.fui-checked') === null) document.querySelectorAll('[data-code=\"DIRECT\"]')[0].querySelector('span').click()"
     txt = """
